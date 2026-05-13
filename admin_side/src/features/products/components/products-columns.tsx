9df9e8a09bc83bec223826b631da3f3cb7157973
Ali Armaghan <armaghanbinali@gmail.com>
2026-05-13 23:50:05 +0500

    feat: implement product table columns with edit and delete action menu

--- admin_side/src/features/products/components/products-columns.tsx
+++ admin_side/src/features/products/components/products-columns.tsx
@@ -12,6 +12,43 @@ import {
 import { Product } from '../types'
 import { useProducts } from '../context/products-context'
 
+// Extracted into a proper React component so hooks can be called safely
+function ProductRowActions({ product }: { product: Product }) {
+  const { setOpen, setCurrentRow } = useProducts()
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant='ghost' className='h-8 w-8 p-0'>
+          <span className='sr-only'>Open menu</span>
+          <MoreHorizontal className='h-4 w-4' />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align='end'>
+        <DropdownMenuLabel>Actions</DropdownMenuLabel>
+        <DropdownMenuItem
+          onClick={() => {
+            setCurrentRow(product)
+            setOpen('edit')
+          }}
+        >
+          <Edit className='mr-2 h-4 w-4' /> Edit
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem
+          className='text-red-600'
+          onClick={() => {
+            setCurrentRow(product)
+            setOpen('delete')
+          }}
+        >
+          <Trash className='mr-2 h-4 w-4' /> Delete
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
+
 export const productsColumns: ColumnDef<Product>[] = [
   {
     accessorKey: 'name',
@@ -51,41 +88,6 @@ export const productsColumns: ColumnDef<Product>[] = [
   },
   {
     id: 'actions',
-    cell: ({ row }) => {
-      const product = row.original
-      const { setOpen, setCurrentRow } = useProducts()
-
-      return (
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant='ghost' className='h-8 w-8 p-0'>
-              <span className='sr-only'>Open menu</span>
-              <MoreHorizontal className='h-4 w-4' />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align='end'>
-            <DropdownMenuLabel>Actions</DropdownMenuLabel>
-            <DropdownMenuItem
-              onClick={() => {
-                setCurrentRow(product)
-                setOpen('edit')
-              }}
-            >
-              <Edit className='mr-2 h-4 w-4' /> Edit
-            </DropdownMenuItem>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem
-              className='text-red-600'
-              onClick={() => {
-                setCurrentRow(product)
-                setOpen('delete')
-              }}
-            >
-              <Trash className='mr-2 h-4 w-4' /> Delete
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      )
-    },
+    cell: ({ row }) => <ProductRowActions product={row.original} />,
   },
 ]
